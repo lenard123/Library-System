a24f582cafc2713dee22b1b3ff50b4c9a948976e
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2018-11-30 10:13:02 +0800

    git commit -m "Updating book Feature Added"
    Signed-off-by: Lenard Mangay-ayam <lenard.mangayayam@gmail.com>

--- pages/book/update.php
+++ pages/book/update.php
@@ -6,7 +6,49 @@
     <h1>Update Books</h1>
     <hr/>
 
-    
+    <?php if ($notfound) : ?>
+        <div class="jumbotron">
+            <h1>Book no found</h1>
+            <h3>Click <a href="<?= _public("pages/book") ?>">here</a> to go back.</h3>
+        </div>
+    <?php else : ?>
+
+        <div class="card col-md-6">
+            <div class="card-body">
+                <form method="POST" action="">
+
+                    <?= showSuccess() ?>
+                    <?= showErrors() ?>
+
+                    <div class="form-group">
+                        <label>Title</label>
+                        <input type="text" name="title" class="form-control" value="<?= $book["title"] ?>" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Author</label>
+                        <input type="text" name="author" class="form-control" value="<?= $book["author"] ?>" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Stocks in Library</label>
+                        <input type="number" name="quantity" class="form-control" value="<?= $book["quantity"] ?>" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Short Description</label>
+                        <textarea class="form-control" name="description"><?= $book["description"] ?></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" value="Submit" class="btn btn-success">
+                        <a href="<?= _public("pages/book#book-$id") ?>" class="btn btn-outline-secondary">Back</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    <?php endif ?>    
 
 
 </div>
--- scripts/book/update.php
+++ scripts/book/update.php
@@ -1,7 +1,8 @@
 <?php
 
 require_once __DIR__."/../../Conn.php";
-require_once __DIR__."/../../Book.php";
+require_once __DIR__."/../../Helper.php";
+require_once __DIR__."/../../Result.php";
 
 if (!isset($_GET["id"])) {
     redirect("book");
@@ -17,5 +18,53 @@ $result = $stmt->get_result();
 if ($result->num_rows < 1) {
     $notfound = TRUE;
 } else {
-    $book = $result->get_result();
+    $book = $result->fetch_assoc();
+    $borrowed = $conn->query("SELECT * FROM `logs` WHERE `book_id`=$id AND `returned_date` IS NULL")
+                     ->num_rows;
+    $book["quantity"] = $book["quantity"] - $borrowed;
+
+    if (isset($_POST["title"]) && isset($_POST["author"]) && isset($_POST["quantity"]) && isset($_POST["description"])) {
+
+        $title = trim($_POST["title"]);
+        $author = trim($_POST["author"]);
+        $quantity = trim($_POST["quantity"]) + $borrowed;
+        $description = trim($_POST["description"]);
+
+        if (empty($title)) {
+            addError("Title is required");
+        } 
+
+        if (empty($quantity)) {
+            addError("Quantity is required");
+        }
+
+        if (!empty($quantity) && !is_int($quantity)) {
+            addError("Quantity must be a number");
+        }
+
+        $stmt = $conn->prepare("SELECT * FROM `books` WHERE `title`=? AND `author`=? AND `id`<>$id");
+        $stmt->bind_param("ss", $title, $author);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows >= 1) {
+            addError("A book with the same title and author already exists");
+        }
+
+        if (!hasError()) {
+            $stmt = $conn->prepare("UPDATE `books` SET `title`=?, `author`=?, `quantity`=?, `description`=? WHERE `id`=$id");
+            $stmt->bind_param("ssis", $title, $author, $quantity, $description);
+            if ($stmt->execute()) {
+                addSuccess("Book updated successfully");
+                $book["title"] = $title;
+                $book["author"] = $author;
+                $book["quantity"] = $quantity-$borrowed;
+                $book["description"] = $description;
+            } else {
+                addError("An error occured while updating the book");
+            }
+        }
+
+    }
+
 }
+
